acf6c599839be4a1497782b05f7e9a97c8b40871
houssem3301 <houssem3301@gmail.com>
2024-05-10 00:21:28 +0400

    Init
    Init Project-Minimal Version

---
+++ Alpha.WebcamYolo/Prediction.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Alpha.WebcamYolo
+{
+    public class Prediction
+    {
+        public string? Label { get; init; }
+        public RectangleF Rectangle { get; init; }
+        public float Score { get; init; }
+    }
+}
