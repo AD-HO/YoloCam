8518090a7e4bf60318dc334ff493915b86d24c3a
houssem3301 <houssem3301@gmail.com>
2024-05-11 11:14:09 +0400

    Adding appsettings.json
    Adding Configuration of Model selection and Cuda Usage

--- YoloWPF/MainWindow.xaml.cs
+++ YoloWPF/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Microsoft.Extensions.Configuration;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System.Configuration;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Text;
@@ -32,38 +34,71 @@ namespace YoloWPF
         private CancellationTokenSource _webcamCancellationTokenSource;
         private CancellationToken _webcamCancellationToken;
 
+        public IConfiguration Configuration { get; private set; }
+
+
         public MainWindow()
         {
-            _yolo = new Yolo("./yolov8s.onnx",cuda:false);
+            var builder = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+            var modelPath = Configuration.GetSection("Model").Value;
+            var cudaUsage = bool.Parse(Configuration.GetSection("CudaUsage").Value);
+
+
+            if (cudaUsage)
+
+                try
+                {
+
+                    _yolo = new Yolo(modelPath, cuda: true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during Inferecing with cuda, Moving to CPU !");
+                    MessageBox.Show(ex.Message);
+                    _yolo = new Yolo(modelPath, cuda: false);
+                }
+            else
+                _yolo = new Yolo(modelPath, cuda: false);
+
+
+
             _dispatcher = Dispatcher.CurrentDispatcher;
+            _webcamCancellationTokenSource = new CancellationTokenSource();
+            _webcamCancellationToken = _webcamCancellationTokenSource.Token;
+            //Invoke webcam in a new thread
+            Task.Run(() => WebcamAsync(_webcamCancellationToken), _webcamCancellationToken);
             InitializeComponent();
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-       
-            _webcamCancellationTokenSource=new CancellationTokenSource();
-            _webcamCancellationToken= _webcamCancellationTokenSource.Token;
+
+            _webcamCancellationTokenSource = new CancellationTokenSource();
+            _webcamCancellationToken = _webcamCancellationTokenSource.Token;
             //Invoke webcam in a new thread
-            Task.Run(()=> WebcamAsync(_webcamCancellationToken), _webcamCancellationToken);
+            Task.Run(() => WebcamAsync(_webcamCancellationToken), _webcamCancellationToken);
         }
         private async Task WebcamAsync(CancellationToken cancellationToken)
         {
             try
             {
-                using var  capture = new VideoCapture( 0, VideoCapture.API.DShow);
-                capture.Set(property:CapProp.FrameCount, value:30);
-                capture.Set(property:CapProp.FrameWidth, value:640);
-                capture.Set(property:CapProp.FrameHeight, value:640);
+                using var capture = new VideoCapture(0, VideoCapture.API.DShow);
+                capture.Set(property: CapProp.FrameCount, value: 30);
+                capture.Set(property: CapProp.FrameWidth, value: 640);
+                capture.Set(property: CapProp.FrameHeight, value: 640);
 
-                using  MemoryStream? stream = new MemoryStream();
+                using MemoryStream? stream = new MemoryStream();
 
-                while(cancellationToken.IsCancellationRequested is false)
+                while (cancellationToken.IsCancellationRequested is false)
                 {
                     capture.QueryFrame().ToBitmap().Save(stream, format: ImageFormat.Bmp);
                     stream.Position = 0;
-                    using  Image<Bgra32> img =  await Image.LoadAsync<Bgra32> (stream);
-                     List<ObjectDetection?> results= _yolo.RunObjectDetection(img);
+                    using Image<Bgra32> img = await Image.LoadAsync<Bgra32>(stream);
+                    List<ObjectDetection?> results = _yolo.RunObjectDetection(img);
                     img.Draw(results);
                     await _dispatcher.Invoke(async () => WebcamImage.Source = await ImageSharpToBitmapAsync(img));
 
@@ -73,8 +108,8 @@ namespace YoloWPF
             }
             catch (Exception ex)
             {
-
-                throw;
+                MessageBox.Show(ex.Message);
+                throw ex;
             }
         }
 
@@ -91,14 +126,6 @@ namespace YoloWPF
             return bitmap;
         }
 
-        private void StopButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-
-        }
+ 
     }
 }
